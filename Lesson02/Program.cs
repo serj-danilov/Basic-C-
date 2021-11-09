a4b3a6b59502a000e87bc6ecf6517136f4f952e5
Sergey Danilov <serj.danilov@gmail.com>
2021-11-09 19:13:07 +0300

    Lesson02

---
+++ Lesson02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson02
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
